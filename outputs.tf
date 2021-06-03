8649ba39beda9a37985b654bab334902fec5f55d
Yash Khemani <khemani@m13.home.seva.cafe>
2021-06-03 03:38:11 +0000

    mark kube_config as sensitive

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,8 @@ output "client_certificate" {
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config_raw
+  value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
+  sensitive = true
 }
 
 output "host" {
